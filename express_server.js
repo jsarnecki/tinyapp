1ee4e75a3bd67446263819eb0808fe8352e91b8d
Josh Sarnecki <joshsarnecki@gmail.com>
2022-02-21 23:02:59 +0000

    Set view engine to ejs

--- express_server.js
+++ express_server.js
@@ -7,14 +7,39 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.set('view engine', 'ejs');
+
+//index page
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.render("pages/index");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+//about page
+app.get('/about', (req, res) => {
+  res.render('pages/about');
 });
+
+//Use <%- include('RELATIVE/PATH/TO/FILE') %> to embed an EJS partial in another file.
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+// app.get('/set', (req, res) => {
+//   const a = 1;
+//   res.send(`a: ${a}`);
+// });
+
+// app.get('/fetch', (req, res) => {
+//   res.send(`a, again: ${a}`);
+// })
